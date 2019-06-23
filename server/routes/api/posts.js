870603f64f08fcd8470ed6813efcf74aca062e23
Volodza <looka66@mail.ru>
2019-06-23 23:21:11 +0300

    Backend created

---
+++ server/routes/api/posts.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+router.get('/', async (req,res) => {
+    const posts = await getPosts();
+    res.send(posts.find({}).toArray())
+});
+
+router.post('/', async (req,res) => {
+    const posts = await getPosts();
+    await posts.insertOne({
+        text:req.body.text,
+        created:new Date()
+    })
+    res.status(201).send();
+});
+
+router.delete('/:id', async (req,res) => {
+    const posts = await getPosts();
+    await posts.deleteOne({_id:new mongodb.ObjectID(req.params.id)})
+    res.status(200).send();
+})
+
+
+async function getPosts () {
+    const uri = "mongodb+srv://abc123:Vkfltytwjou9df9@vueexpress-nq8xi.mongodb.net/test?retryWrites=true&w=majority";
+    const client = await mongodb.MongoClient.connect(uri,{ useNewUrlParser: true })
+
+    return client.db("vueExpress").collection("posts");
+}
+
+module.exports = router;
